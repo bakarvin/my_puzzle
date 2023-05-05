82e7fffc395d458ed14c9008b182457156854201
bakarvin <arviin.maulana17@gmail.com>
2023-05-05 14:36:50 +0700

    first commit

---
+++ app/src/main/java/com/example/mypuzzle/FlipScreen.kt
@@ -0,0 +1,42 @@
+data class CardModel(
+    val text: String,
+    val isFlipped: Boolean
+)
+
+var list = listOf<CardModel>(
+    CardModel(
+        text = "Prof",
+        isFlipped = false
+    ),
+    CardModel(
+        text = "Prof",
+        isFlipped = false
+    ),
+    CardModel(
+        text = "Doc",
+        isFlipped = false
+    ),
+    CardModel(
+        text = "Doc",
+        isFlipped = false
+    ),
+    CardModel(
+        text = "Dog",
+        isFlipped = false
+    ),CardModel(
+        text = "Dog",
+        isFlipped = false
+    ),
+    CardModel(
+        text = "Cow",
+        isFlipped = false
+    ),
+    CardModel(
+        text = "Cow",
+        isFlipped = false
+    ),
+    CardModel(
+        text = "A",
+        isFlipped = false
+    ),
+)
